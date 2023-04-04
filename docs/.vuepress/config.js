feacdc41d5af0cbda59b565be316611cb3ac50d2
zwb <email@sample.com>
2023-04-04 11:33:25 +0800

    iii

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,13 @@
 module.exports = {
     title: "淡水海边",
     description: "分享各类资源、教程等等",
+    base: '/knowledge-github/',
     markdown: {
         lineNumbers: true,
     },
     themeConfig: {
         nav: [
-            { text: "基础教程", link: "/vue2/" },
+            { text: "基础指南", link: "/vue2/" },
             { text: "联系作者", link: "/guide/concact" },
             { text: "github", link: "https://github.com/JAY-825/knowledge-gihub" }
             // { text: "gitee", link: "https://gitee.com/Rewind_ljp/knowledge" }
@@ -19,6 +20,9 @@ module.exports = {
                     ['vue2/', "vue2特点介绍"],
                     ['vue2/spa', "对单页面应用(spa)的理解"],
                     ['vue2/ifandshow', "v-if和v-show的区别"],
+                    ['vue2/life', "vue2生命周期"],
+                    ['vue2/data', "vue中的data属性"],
+                    ['vue2/cor', "vue组件通信方式"],
                 ]
             },
             {
--- docs/vue2/ifandshow.md
+++ docs/vue2/ifandshow.md
@@ -1,1 +1,44 @@
-# 谈谈vue中v-if与v-show的区别
+# vue中的v-if与v-show
+
+## 相同点
+v-if和v-show在效果上都可以控制元素是否显示，当条件成立时(true)，显示元素;当条件不成立时(false),不显示元素
+
+基本用法
+```js
+// html
+<div v-if="isshow">
+<div v-show="isshow">
+
+// script
+data(){
+    return { isshow: true }
+}
+```
+
+## 主要区别
+**控制手段不同：**
+
+v-show隐藏则是为该元素添加display:none，dom元素依旧还在。v-if显示隐藏是将dom元素整个添加或删除
+
+**编译过程不同：**
+
+v-if切换有一个局部编译/卸载的过程，切换过程中合适地销毁和重建内部的事件监听和子组件；v-show只是简单的基于css切换
+
+**编译条件不同：**
+
+v-if是真正的条件渲染，它会确保在切换过程中条件块内的事件监听器和子组件适当地被销毁和重建。只有渲染条件为假时，并不做操作，直到为真才渲染
+
+**当使用在组件上时：**
+
+`v-show` 由false变为true的时候不会触发组件的生命周期
+
+`v-if` 由false变为true的时候，触发组件的`beforeCreate`、`created`、`beforeMount`、`mounted`钩子，由true变为false的时候触发组件的`beforeDestory`、`destoryed`方法
+
+
+## 使用场景
+v-show是通过css的display属性控制元素显隐，v-if是通过条件控制元素是否渲染。
+在性能上，`v-if`有更高的切换消耗，`v-show`有更高的初始渲染消耗
+
+若需要频繁地切换元素显隐状态，使用 v-show 较好
+
+如果在运行时条件很少改变，使用 v-if 较好
